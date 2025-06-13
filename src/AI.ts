e674dae52448babdb62f6ad70be3d8cb81989f01
Bruno Moretti <brmoretti@gmail.com>
2025-06-12 21:18:52 -0300

    AI implemmentation done

--- src/AI.ts
+++ src/AI.ts
@@ -93,58 +93,6 @@ export class AI {
 		return predictedY;
 	}
 
-	predict2(ball: Ball) { //complex but easier
-		if (ball.currentXSpeed === 0 && ball.currentYSpeed === 0) {
-			this.aiTargetY = Board.height / 2;
-			return;
-		}
-
-		if (ball.currentXSpeed > 0) {
-			const timeToReach = (this.aiX - ball.currentX - 2 * Ball.radius) / ball.currentXSpeed;
-			let predictedY = ball.currentY + (ball.currentYSpeed * timeToReach);
-
-			while (predictedY < 0 || predictedY > Board.height) {
-				if (predictedY < 0) {
-					predictedY = -predictedY;
-				}
-				if (predictedY > Board.height) {
-					predictedY = 2 * Board.height - predictedY;
-				}
-			}
-			this.aiTargetY = predictedY + Ball.radius;
-		} else {
-			const timeToOpponent = (this.opponentX - ball.currentX) / ball.currentXSpeed;
-			let bounceY = ball.currentY + (ball.currentYSpeed * timeToOpponent);
-
-			while (bounceY < 0 || bounceY > Board.height) {
-				if (bounceY < 0) {
-					bounceY = -bounceY;
-				}
-				if (bounceY > Board.height) {
-					bounceY = 2 * Board.height - bounceY;
-				}
-			}
-
-			const newXSpeed = -ball.currentXSpeed;
-			const newYSpeed = ball.currentYSpeed;
-
-			const timeToAI = (this.aiX - this.opponentX - 2 * Ball.radius) / newXSpeed;
-			let finalPredictedY = bounceY + (newYSpeed * timeToAI);
-
-
-			while (finalPredictedY < 0 || finalPredictedY > Board.height) {
-				if (finalPredictedY < 0) {
-					finalPredictedY = -finalPredictedY;
-				}
-				if (finalPredictedY > Board.height) {
-					finalPredictedY = 2 * Board.height - finalPredictedY;
-				}
-			}
-
-			this.aiTargetY = finalPredictedY + Ball.radius;
-		}
-	}
-
 	movePaddle() {
 		const current_y: number = this.aiPlayer.y + (Paddle.height / 2);
 		const tolerance = 5;
